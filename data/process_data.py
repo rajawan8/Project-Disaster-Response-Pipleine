8ff24149dfdbf003860dc8f2c321c0a7ebbf816b
rajawan8 <108656565+rajawan8@users.noreply.github.com>
2022-11-02 07:08:40 +0000

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -1,16 +1,93 @@
+# Importing Libraries
 import sys
-
+import os
+import pandas as pd
+from sqlalchemy import create_engine
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 def load_data(messages_filepath, categories_filepath):
-    pass
+    '''
+    Loads and merges datasets
+    Args:
+    messages_filepath: String. Filepath for the csv file containing the messages.
+    categories_filepath: String. Filepath for the csv file containing the categories.
+    Returns:
+    df: pandas dataframe. Dataframe containing messages and respective categories.
+    '''
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    
+    print('Messages dataframe')
+    messages.head()
+    print('Categories dataframe')
+    categories.head()
+    
+    df = pd.merge(messages, categories, on='id', how='outer')
+    
+    return df
 
+def clean_data(df):  
+    '''
+    Clean dataframes from uneeded columns, duplicates and text artifacts
+    Args:
+    df: pandas dataframe. Dataframe containing messages and categories.
+    Returns:
+    df: pandas dataframe. Dataframe containing cleaned version of messages and categories.
+    '''
+    categories = df.categories.str.split(';', expand=True)
+    
+    # select the first row of the categories dataframe
+    row = categories.iloc[[1]]
+    
+    # use this row to extract a list of new column names for categories.
+    # one way is to apply a lambda function that takes everything 
+    # up to the second to last character of each string with slicing
+    category_colnames = row.applymap(lambda x: x[:-2]).iloc[0, :].tolist()
+    
+    # rename the columns of `categories`
+    categories.columns = category_colnames
+    
+    # Convert category values to just numbers 0 or 1
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].astype(str).str[-1:]
 
-def clean_data(df):
-    pass
-
+        # convert column from string to numeric
+        categories[column] = categories[column].astype(int)
+        
+    # Based on the figure 8 documentation original mapping is the following: 1 - yes, 2 - no, so I will convert all the 2's to 0's
+    categories['related'] = categories['related'].replace(2, 0)
+    
+    # The child alone column has a single label so that won't be helpful to train my model I will drop that column
+    categories.drop("child_alone", axis=1, inplace=True)
+    
+    # Put processed columns back into the main df
+    # concatenate the original dataframe with the new `categories` dataframe
+    df.drop("categories", axis=1, inplace=True)
+    df = pd.concat([df,categories], axis=1)
+    
+    # Remove duplicates
+    df.drop_duplicates(inplace=True)
+    
+    
+    return df
 
 def save_data(df, database_filename):
-    pass  
+    """
+    Save the cleaned data.
+    
+    Input:
+    df: pandas dataframe. Dataframe containing cleaned version of messages and respective categories.
+    database_filename: String. Filename for the output database.
+    
+    Output:
+    None.
+    """
+    
+    engine = create_engine('sqlite:///'+ database_filename)
+    df.to_sql('messages', engine, index = False, if_exists='replace')
+
 
 
 def main():
@@ -18,8 +95,7 @@ def main():
 
         messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
 
-        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
-              .format(messages_filepath, categories_filepath))
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'.format(messages_filepath, categories_filepath))
         df = load_data(messages_filepath, categories_filepath)
 
         print('Cleaning data...')
